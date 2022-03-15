73a87a94d26d1c0f5c2e94f9e128ebcbbc149460
AA Student <appacademy@Mini-15.local>
2022-03-15 13:43:18 -0700

    Create Employee class

---
+++ employee_manager/employee.rb
@@ -0,0 +1,13 @@
+class Employee
+
+    def initialize(name, title, salary, boss)
+        @name = name 
+        @title = title 
+        @salary = salary
+        @title = title
+    end
+
+    
+
+
+end
