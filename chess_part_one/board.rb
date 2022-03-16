92e0ffc6fb140c8811d3732d8bf10d53c76cbb55
AA Student <appacademy@Mini-15.local>
2022-03-15 17:01:51 -0700

    Add move_piece method

--- chess_part_one/board.rb
+++ chess_part_one/board.rb
@@ -14,12 +14,37 @@ class Board
     def fill_rows
         rows.map.with_index do |row, i|
             if i.between?(2, 5)
-                (0...row.length).each { |j| row[j] = nullpiece }
+                (0...row.length).each { |j| row[j] = nil } # change to nullpiece
             else
-                (0...row.length).each { |k| row[k] = piece }
+                (0...row.length).each { |k| row[k] = "p" } # change to piece
             end
         end 
-        p rows     
+       rows     
+    end
+
+    def [](pos)
+       row, col = pos
+       @rows[row][col]
+    end
+
+    def []=(pos, piece)
+        row, col = pos
+        @rows[row][col] = piece
+    end
+
+    def move_piece(color, start_pos, end_pos) 
+        
+        if self[start_pos] == nil #changet to nullpiece
+            raise "there is no piece here"
+        end
+            
+        # if self[end_pos] != valid_move?(end_pos)     # make valid_move?
+        #     raise "cannot move here"
+        # end
+
+        self[end_pos] = self[start_pos]
+        self[start_pos] = nil  #change to nullpiece
+        p rows
     end
 
 
